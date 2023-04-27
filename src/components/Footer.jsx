b51d45cc472460abd325aeb18fe59704fd020b53
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-04-27 17:19:50 +0530

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,37 +6,37 @@ import { AiFillGithub } from "react-icons/ai";
 const Footer = () => {
     return (
         <footer className="bg-primary py-8">
-            <div className="flex text-white flex-col gap-y-6 lg:flex-row items-center justify-between">
+            <div className="flex text-white flex-col p-4 gap-y-6 lg:flex-row items-center justify-between">
                 <p className="text-white text-center">
                     Copyright &copy;{new Date().getFullYear()} E-com All right
                     reserved
                 </p>
 
-                <div className="text-center ">
+                <div className="text-center flex-xl ">
                     <a
                         href="https://tusharupadhyay.vercel.app/"
                         target="_blank"
-                        className="text-orange-300  hover:text-orange-400"
+                        className="text-orange-200  hover:text-orange-400"
                     >
                         Tushar Upadhyay
                     </a>
 
-                    <div className="flex gap-x-3 mt-3 ">
-                        <div
+                    <div className="flex  gap-x-3 mt-3 text-xl ">
+                        <a
                             href="https://www.linkedin.com/in/tusharupadhya/"
                             target="_blank"
-                            className=" text-orange-300  hover:text-orange-400 cursor-pointer"
+                            className=" text-orange-200  hover:text-orange-400 cursor-pointer"
                         >
                             <AiFillLinkedin />
-                        </div>
+                        </a>
 
-                        <div
-                            href="#"
+                        <a
+                            href="https://github.com/tushar-upadhya/e-com-api"
                             target="_blank"
-                            className="flex gap-x-2  text-orange-300  hover:text-orange-400 cursor-pointer"
+                            className="flex gap-x-2  text-orange-200  hover:text-orange-400 cursor-pointer"
                         >
                             <AiFillGithub />
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
